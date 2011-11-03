f3cf66858bb916dec6ba1e699eb257439dcdf5d5
Enric Meinhardt-Llopis <coco@fuchsia.hw.ipol.im>
2011-11-03 12:27:16 +0100

    middlebury compatibility option for flow field visualization

--- viewflow.c
+++ viewflow.c
@@ -49,6 +49,7 @@ static void viewflow_flat(uint8_t *py, float *px, int w, int h, float m)
 	FORJ(h) FORI(w) {
 		float *v = x[j][i];
 		double r = hypot(v[0], v[1]);
+		if (r > 1e8) { FORL(3) y[j][i][l] = 255; continue; }
 		r = r>m ? 1 : r/m;
 		double a = atan2(v[1], -v[0]);
 		a = (a+M_PI)*(180/M_PI);
@@ -61,6 +62,143 @@ static void viewflow_flat(uint8_t *py, float *px, int w, int h, float m)
 		hsv_to_rgb_doubles(rgb, hsv);
 		FORL(3)
 			y[j][i][l] = 255*rgb[l];
+
+	}
+}
+
+
+int middlebury_ncols = 0;
+#define MIDDLEBURY_MAXCOLS 60
+int middlebury_colorwheel[MIDDLEBURY_MAXCOLS][3];
+
+
+void middlebury_setcols(int r, int g, int b, int k)
+{
+    middlebury_colorwheel[k][0] = r;
+    middlebury_colorwheel[k][1] = g;
+    middlebury_colorwheel[k][2] = b;
+}
+
+void middlebury_makecolorwheel()
+{
+    // relative lengths of color transitions:
+    // these are chosen based on perceptual similarity
+    // (e.g. one can distinguish more shades between red and yellow 
+    //  than between yellow and green)
+    int RY = 15;
+    int YG = 6;
+    int GC = 4;
+    int CB = 11;
+    int BM = 13;
+    int MR = 6;
+    middlebury_ncols = RY + YG + GC + CB + BM + MR;
+    //printf("ncols = %d\n", ncols);
+    if (middlebury_ncols > MIDDLEBURY_MAXCOLS)
+	exit(1);
+    int i;
+    int k = 0;
+    for (i = 0; i < RY; i++) middlebury_setcols(255, 255*i/RY, 0, k++);
+    for (i = 0; i < YG; i++) middlebury_setcols(255-255*i/YG, 255, 0, k++);
+    for (i = 0; i < GC; i++) middlebury_setcols(0, 255, 255*i/GC, k++);
+    for (i = 0; i < CB; i++) middlebury_setcols(0, 255-255*i/CB, 255, k++);
+    for (i = 0; i < BM; i++) middlebury_setcols(255*i/BM, 0, 255, k++);
+    for (i = 0; i < MR; i++) middlebury_setcols(255, 0, 255-255*i/MR, k++);
+}
+
+void middlebury_computeColor(float fx, float fy, unsigned char *pix)
+{
+    if (middlebury_ncols == 0)
+	middlebury_makecolorwheel();
+
+    float rad = sqrt(fx * fx + fy * fy);
+    float a = atan2(-fy, -fx) / M_PI;
+    float fk = (a + 1.0) / 2.0 * (middlebury_ncols-1);
+    int k0 = (int)fk;
+    int k1 = (k0 + 1) % middlebury_ncols;
+    float f = fk - k0;
+    //f = 0; // uncomment to see original color wheel
+    for (int b = 0; b < 3; b++) {
+	float col0 = middlebury_colorwheel[k0][b] / 255.0;
+	float col1 = middlebury_colorwheel[k1][b] / 255.0;
+	float col = (1 - f) * col0 + f * col1;
+	if (rad <= 1)
+	    col = 1 - rad * (1 - col); // increase saturation with radius
+	else
+	    col *= .75; // out of range
+	pix[2 - b] = (int)(255.0 * col);
+    }
+}
+
+// a smart parameter is just like a regular parameter, but it can be
+// re-defined at the shell-environment.  Instead of
+//
+// 	#define NUMBER 42
+// 	...
+// 	printf("%g", NUMBER);
+//
+// do
+// 	SMART_PARAMETER(NUMBER,42)
+// 	...
+// 	printf("%g", NUMBER());
+//
+// Notice that the environment only gets queried once, at the first use.
+//
+#define SMART_PARAMETER(n,v) static double n(void)\
+{\
+	static bool smapa_known_ ## n = false;\
+	static double smapa_value_ ## n = v;\
+	if (!smapa_known_ ## n)\
+	{\
+		fprintf(stderr,"scanning the environment for \"%s\"... ", #n);\
+		int r;\
+		char *sv = getenv(#n);\
+		double y;\
+		if (sv)\
+			r = sscanf(sv, "%lf", &y);\
+		if (sv && r == 1)\
+		{\
+			fprintf(stderr, "got value %g\n", y);\
+			smapa_value_ ## n = y;\
+		} else {\
+			fprintf(stderr, "kept default value %g\n",\
+					smapa_value_ ## n);\
+		}\
+		smapa_known_ ## n = true;\
+	}\
+	return smapa_value_ ## n;\
+}
+
+SMART_PARAMETER(MRANGE,NAN)
+
+static bool middlebury_toolarge(float *v)
+{
+	return fabs(v[0]) > 1e5 || fabs(v[1]) > 1e5;
+}
+
+static void viewflow_middlebury(uint8_t *py, float *px, int w, int h)
+{
+	float (*x)[w][2] = (void*)px;
+	uint8_t (*y)[w][3] = (void*)py;
+	float range = MRANGE();
+	if (!isfinite(range)) {
+		range = -1;
+		FORJ(h) FORI(w) {
+			float *v = x[j][i];
+			if (middlebury_toolarge(v))
+				continue;
+			float nv = hypot(v[0], v[1]);
+			if (nv > range)
+				range = nv;
+		}
+	}
+	fprintf(stderr, "range = %g\n", range);
+	FORJ(h) FORI(w) {
+		float *v = x[j][i];
+		unsigned char pix[3] = {0, 0, 0};
+		if (!middlebury_toolarge(v))
+			middlebury_computeColor(-v[1]/range, -v[0]/range, pix);
+		FORL(3)
+			y[j][i][l] = pix[l];
 
 	}
 }
@@ -127,9 +265,12 @@ int main_viewflow(int c, char *v[])
 	float satscale = atof(v[1]);
 
 	//viewflow_pd(view, flow, w, h, fabs(satscale));
-	viewflow_flat(view[0][0], flow[0][0], w, h, fabs(satscale));
-	if (satscale < 0)
-		overlines(view, flow, w, h, -satscale);
+	if (isfinite(satscale)) {
+		viewflow_flat(view[0][0], flow[0][0], w, h, fabs(satscale));
+		if (satscale < 0)
+			overlines(view, flow, w, h, -satscale);
+	} else
+		viewflow_middlebury(view[0][0], flow[0][0], w, h);
 
 	iio_save_image_uint8_vec(outfile, view[0][0], w, h, 3);
 
