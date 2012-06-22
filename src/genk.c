2ebe0c761b6ce6e4bf9b9a24dda77382fd1599b3
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2012-06-22 11:50:44 +0200

    zeropad

---
+++ src/genk.c
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "fail.c"
+#include "xmalloc.c"
+#include "random.c"
+
+#include "smapa.h"
+
+static int good_modulus(int n, int p)
+{
+	if (p < 1)
+		fail("bad modulus %d\n", p);
+
+	int r;
+	if (n >= 0)
+		r = n % p;
+	else {
+		r = p - (-n) % p;
+		if (r == p)
+			r = 0;
+	}
+	if (r < 0 || r >= p)
+		fprintf(stderr, "mod(%d,%d)=%d\n",n,p,r);
+	assert(r >= 0);
+	assert(r < p);
+	return r;
+}
+
+static void addsample_periodic(float *x, int w, int h, int i, int j, float inc)
+{
+	int gi = good_modulus(i, w);
+	int gj = good_modulus(j, h);
+	x[gj*w+gi] += inc;
+}
+
+SMART_PARAMETER_SILENT(SEED,0)
+
+static void build_k(float *x, int w, int np, float var, int nc)
+{
+	srand(SEED());
+	float p[np][2], c[2] = {0, 0};
+	for (int i = 0; i < w*w; i++)
+		x[i] = 0;
+	for (int i = 0; i < np; i++)
+	{
+		p[i][0] = var*random_normal();
+		p[i][1] = var*random_normal();
+		c[0] += p[i][0] / np;
+		c[1] += p[i][1] / np;
+	}
+	for (int i = 0; i < np; i++)
+		addsample_periodic(x, w, w, p[i][0]-c[0], p[i][1]-c[1], 1);
+	addsample_periodic(x, w, w, 0, 0, nc);
+}
+
+#include "iio.h"
+
+int main(int c, char *v[])
+{
+	if (c != 6 && c != 5) {
+		fprintf(stderr, "usage:\n\t%s kw np var nc [out]\n", *v);
+		//                         0  1  2  3   4   5
+		return EXIT_FAILURE;
+	}
+	int kw = atoi(v[1]);
+	int np = atoi(v[2]);
+	float var = atof(v[3]);
+	int nc = atoi(v[4]);
+	char *filename_out = c > 5 ? v[5] : "-";
+
+	int w = 2*kw + 1;
+	float *x = xmalloc(w * w * sizeof*x);
+	build_k(x, w, np, var, nc);
+	iio_save_image_float(filename_out, x, w, w);
+	return EXIT_SUCCESS;
+}
---
+++ src/zeropad.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "xmalloc.c"
+
+#include "iio.h"
+
+int main(int c, char *v[])
+{
+	if (c != 3 && c != 4 && c != 5) {
+		fprintf(stderr, "usage:\n\t%s mx my [in [out]]\n", *v);
+		//                          0 1  2  3   4
+		return EXIT_FAILURE;
+	}
+	int mx = atoi(v[1]);
+	int my = atoi(v[2]);
+	char *filename_in = c > 3 ? v[3] : "-";
+	char *filename_out = c > 4 ? v[4] : "-";
+
+	int w, h, pd;
+	float *x_raw = iio_read_image_float_vec(filename_in, &w, &h, &pd);
+	float *y_raw = xmalloc( (w + mx)*(h + my)*pd*sizeof*y_raw);
+	float (*x)[w][pd] = (void*)x_raw;
+	float (*y)[w+mx][pd] = (void*)y_raw;
+
+	for (int j = 0; j < h+my; j++)
+	for (int i = 0; i < w+mx; i++)
+	for (int l = 0; l < pd; l++)
+		y[j][i][l] = (i<w && j<h) ? x[j][i][l] : 0;
+
+	iio_save_image_float_vec(filename_out, y_raw, w+mx, h+my, pd);
+	free(x);
+	free(y);
+	return EXIT_SUCCESS;
+}
