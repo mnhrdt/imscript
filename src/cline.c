c359f4ec69c5c4887a73cf26e6c71323ccc16c02
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2012-12-16 16:30:00 +0100

    display gaussian approximation of histograms

--- src/cline.c
+++ src/cline.c
@@ -6,7 +6,13 @@
 #include "bicubic.c"
 #include "bilinear_interpolation.c"
 
-static void cline(float *l, int n, float *x, int w, int h, float angle)
+static float ipos(int i, int n)
+{
+	return 2*3.1416*(i - n/2.0)/n;
+}
+
+static void cline(float *musigma,
+		float *l, int n, float *x, int w, int h, float angle)
 {
 	float an = angle*3.1416/180;
 	float c = cos(an);
@@ -23,14 +29,36 @@ static void cline(float *l, int n, float *x, int w, int h, float angle)
 	fprintf(stderr, "dir = %g %g\n", dir[0], dir[1]);
 	fprintf(stderr, "zer = %g %g\n", zer[0], zer[1]);
 	fprintf(stderr, "(%g %g)=>(%g %g)\n",from[0],from[1],toto[0],toto[1]);
+	double mu = 0, sigma = 0, nn = 0;
 	for (int i = 0; i < n; i++) {
-		float a = i/(n - 1.0);
-		float p[2];
+		float a = i/(n - 1.0), p[2];
 		for (int j = 0; j < 2; j++)
 			p[j] = (1-a)*from[j] + a*toto[j];
 		//bicubic_interpolation(l + i, x, w, h, 1, p[0], p[1]);
 		bilinear_interpolation_vec_at(l + i, x, w, h, 1, p[0], p[1]);
+		if (isfinite(l[i])) {
+			//double posi = hypot(p[0]-zer[0], p[1]-zer[1]);
+			//if (p[0] < 0) posi = -posi;
+			double posi = ipos(i, n);
+			mu += l[i]*posi;
+			nn += l[i];
+		}
 	}
+	mu /= nn;
+	for (int i = 0; i < n; i++) {
+		float a = i/(n - 1.0), p[2];
+		for (int j = 0; j < 2; j++)
+			p[j] = (1-a)*from[j] + a*toto[j];
+		double posi = ipos(i, n);
+		if (isfinite(l[i]))
+			sigma += l[i]*(posi-mu)*(posi-mu);
+	}
+	sigma = sqrt(sigma/nn);
+	if (musigma) {
+		musigma[0] = mu;
+		musigma[1] = sigma;
+	}
+	fprintf(stderr, "mu sigma = %g %g\n", mu, sigma);
 }
 
 static void clineh(float *l, int n, float *x, int w, int h)
@@ -61,15 +89,31 @@ static void clineh(float *l, int n, float *x, int w, int h)
 			l[i] /= lc[i];
 }
 
-static void plot_cline(float *l, int n, char *title)
+static void plot_cline(float *l, int n, char *title, float mu, float sigma)
 {
 	if (title)
 		printf("set title \"%s\"\n", title);
-	printf("plot \"-\" w lines\n");
+	printf("set samples 1000\n");
+	printf("plot \"-\" w lines title \"data\"");
+	if (isfinite(mu) && sigma > 0) {
+		double mass = 0;
+		for (int i = 0; i < n; i++)
+			if (isfinite(l[i]))
+				mass += l[i];
+		mass *= 2*3.1416/n;
+		float alpha_g = mass/(sigma*sqrt(2*3.1416));
+		float alpha_l = mass/(sigma*sqrt(2));
+		fprintf(stderr, "mass = %g\n", mass);
+		printf(",%g*exp(-(x-%g)**2/(2*(%g)**2)) title \"normal\"",
+				alpha_g, mu, sigma);
+		printf(",%g*exp(-abs(x-%g)*sqrt(2)/%g) title \"laplacian\"",
+				alpha_l, mu, sigma);
+	}
+	printf("\n");
 	for (int i = 0; i < n; i++)
 	{
-		float ipos = 2*3.1416*(i - n/2.0)/n;
-		printf("\t%g %g\n", ipos, l[i]);
+		float ipoz = ipos(i, n);
+		printf("\t%g %g\n", ipoz, l[i]);
 	}
 	printf("end\n");
 }
@@ -91,12 +135,12 @@ int main(int c, char *v[])
 	int n = NFAC()*(2+hypot(w+2,h+2));
 	fprintf(stderr, "geometry = %dx%d\n", w, h);
 	fprintf(stderr, "n = %d\n", n);
-	float l[n];
+	float l[n], musigma[2];
 	if (isfinite(angle))
-		cline(l, n, x, w, h, angle);
+		cline(musigma, l, n, x, w, h, angle);
 	else
 		clineh(l, n, x, w, h);
-	plot_cline(l, n, v[1]);
+	plot_cline(l, n, v[1], musigma[0], musigma[1]);
 	free(x);
 	return 0;
 }
