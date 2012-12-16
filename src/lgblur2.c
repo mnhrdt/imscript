0b9bee41d59f552880128f7fd559e62825a01a53
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2012-12-16 16:38:41 +0100

    nonuniform local blurs

---
+++ src/lgblur2.c
@@ -0,0 +1,159 @@
+// local lgblur (a section of the scale space)
+// this version is optimized por generating multiple versions of the same image
+// usage: lgblur2 infile.png varpat outpat FIRST LAST
+
+
+#include <math.h>
+#include <stdio.h>
+
+static float unnormalized_gaussian_function(float sigma, float x)
+{
+	return exp(-x*x/(2*sigma*sigma));
+}
+
+#define KWMAX 39
+
+static int gaussian_kernel_width(float sigma)
+{
+	float radius = 3 * fabs(sigma);
+	int r = ceil(1 + 2*radius);
+	if (r < 1) r = 1;
+	if (r > KWMAX) r = KWMAX;
+	return r;
+}
+
+static void fill_gaussian_kernel(float *k, int w, float s)
+{
+	int c = (w - 1)/2;
+	for (int j = 0; j < w; j++)
+	for (int i = 0; i < w; i++)
+		k[j*w+i] = unnormalized_gaussian_function(s, hypot(i-c,j-c));
+//
+//	float m = 0;
+//	for (int i = 0; i < w*w; i++)
+//		m += k[i];
+//	for (int i = 0; i < w*w; i++)
+//		k[i] /= m;
+}
+
+static void fill_square(float *s, int ns, float *x, int w, int h, int i, int j)
+{
+	for (int q = 0; q < ns; q++)
+	for (int p = 0; p < ns; p++)
+	{
+		int ii = i + p - (ns - 1)/2;
+		int jj = j + q - (ns - 1)/2;
+		float g = NAN;
+		if (ii >= 0 && jj >= 0 && ii < w && jj < h)
+			g = x[jj*w+ii];
+		s[q*ns+p] = g;
+	}
+}
+
+float local_gaussian_blur_at(float s, float *x, int w, int h, int i, int j)
+{
+	if (s < 0.1) return x[j*w+i];
+	int kw = gaussian_kernel_width(s);
+	float k[kw*kw], c[kw*kw];
+	fill_gaussian_kernel(k, kw, s);
+	fill_square(c, kw, x, w, h, i, j);
+
+	float m = 0;
+	for (int l = 0; l < kw*kw; l++)
+		if (isfinite(c[l]))
+			m += k[l];
+	for (int l = 0; l < kw*kw; l++)
+		k[l] /= m;
+
+	float r = 0;
+	for (int l = 0; l < kw*kw; l++)
+		if (isfinite(c[l]))
+			r += k[l]*c[l];
+
+	return r;
+}
+
+void local_gaussian_blur(float *y, float *s, float *x, int w, int h, int pd)
+{
+	for (int l = 0; l < pd; l++)
+	for (int j = 0; j < h; j++)
+	for (int i = 0; i < w; i++)
+		y[w*h*l+(j*w+i)]
+			= local_gaussian_blur_at(s[j*w+i], x+w*h*l, w,h, i,j);
+}
+
+#define OMIT_GBLUR_MAIN
+#include "gblur.c"
+
+static void fill_gblur_gray(float *y, float *x, int w, int h, float s)
+{
+	gblur_gray(y, x, w, h, s);
+}
+
+static void fill_gblur_split(float *y, float *x, int w, int h, int pd, float s)
+{
+	// assumes "split", not "vec"
+	for (int i = 0; i < pd; i++)
+		fill_gblur_gray(y + i*w*h, x + i*w*h, w, h, s);
+}
+
+
+// fill a pyramid of images of the same size
+void pyrss(float *p, float *x, int w, int h, int pd, int n, float sstep)
+{
+	assert(sstep > 1);
+
+	float s = 1;
+	for (int i = 0; i < n; i++)
+	{
+		fprintf(stderr, "s[%d] = %g\n", i, s);
+		fill_gblur_split(p + i*w*h*pd, x, w, h, pd, s);
+		s *= sstep;
+	}
+}
+
+// utility headers used only in the "main" function
+#include <stdio.h>
+
+#include "iio.h"
+#include "fail.c"
+#include "xmalloc.c"
+
+int main(int c, char *v[])
+{
+	if (c != 6) {
+		fprintf(stderr, "usage:\n\t"
+				"%s in.png varpat outpat FIRST LAST\n", *v);
+		//                0 1      2      3      4     5
+		return 1;
+	}
+	char *filename_in = v[1];
+	char *filepattern_var = v[2];
+	char *filepattern_out = v[3];
+	int first = atoi(v[4]);
+	int last = atoi(v[5]);
+
+	int w, h, pd;
+	float *x = iio_read_image_float_split(filename_in, &w, &h, &pd);
+	float *y = xmalloc(w*h*pd*sizeof*y);
+
+	for (int i = first; i < last; i++)
+	{
+		char filename_var[FILENAME_MAX];
+		char filename_out[FILENAME_MAX];
+		snprintf(filename_var, FILENAME_MAX, filepattern_var, i);
+		snprintf(filename_out, FILENAME_MAX, filepattern_out, i);
+
+		int ww, hh;
+		float *sigma = iio_read_image_float(filename_var, &ww, &hh);
+		if (w != ww || h != hh)
+			fail("variances and image sizes mismatch");
+
+		local_gaussian_blur(y, sigma, x, w, h, pd);
+		iio_save_image_float_split(filename_out, y, w, h, pd);
+		free(sigma);
+	}
+
+	free(x); free(y);
+	return 0;
+}
