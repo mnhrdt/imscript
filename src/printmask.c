e063ebc8b72f37d67411c163eaf7ba67b1058361
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2013-02-15 17:07:37 +0100

    further missing files

---
+++ src/printmask.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "iio.h"
+
+int main()
+{
+	int w, h;
+	float *x = iio_read_image_float("-", &w, &h);
+	for (int j = 0; j < h; j++)
+	for (int i = 0; i < w; i++)
+		if (x[w*j+i] > 0)
+			printf("%d %d\n", i, j);
+	return 0;
+}
