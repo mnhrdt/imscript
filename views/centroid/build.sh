94659c5937b767cb130b374308b257d9c19e61e9
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2013-09-26 00:07:21 +0200

    add makefile to centroid view

--- views/centroid/build.sh
+++
@@ -1,2 +0,0 @@
-c99 -g centroid.c iio.c optical_flow/optical_flow.c optical_flow/pyramidal_horn_schunck.c optical_flow/smooth_inpainting.c -ltiff -lpng -lm -fopenmp
-#c99 -O3 centroid.c iio.c optical_flow/optical_flow.c optical_flow/pyramidal_horn_schunck.c optical_flow/smooth_inpainting.c -ltiff -lpng -lm -fopenmp
