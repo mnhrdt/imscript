6b68fb9f7e3b1aa91ee2a4a162e0d4184b27405f
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2015-04-03 17:04:14 +0200

    (parse numbers bug)

--- src/parsenumbers.c
+++ src/parsenumbers.c
@@ -146,10 +146,12 @@ static double *alloc_parse_doubles(int nmax, const char *ss, int *n)
 	return r;
 }
 
-// obtain n numbers from string
-// the string contains a list of ascii numbers, 
-// or the name of a file containing a list of ascii numbers
-static void read_n_doubles_from_string(double *out, char *string, int n)
+// Obtain n numbers from string.
+// The string contains a list of ascii numbers
+// or the name of a file containing a list of ascii numbers.
+// In case of failure, unread numbers are set to zero.
+// Returns the number of read numbers.
+static int read_n_doubles_from_string(double *out, char *string, int n)
 {
 	for (int i = 0; i < n; i++)
 		out[i] = 0;
@@ -168,6 +170,7 @@ static void read_n_doubles_from_string(double *out, char *string, int n)
 	for (int i = 0; i < no; i++)
 		out[i] = buf[i];
 	free(buf);
+	return no;
 }
 
 #endif//_PARSENUMBERS_C
