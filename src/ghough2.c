bb44d3955291b776c98e3b0e1cf1129cc3fcf3f7
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2015-07-08 08:59:42 +0200

    [ghough2] optimized vote folding

--- src/ghough2.c
+++ src/ghough2.c
@@ -7,14 +7,44 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-static bool global_fold = 0;
-
 static int insideP(int w, int h, int i, int j)
 {
 	return i >= 0 && j >= 0 && i < w && j < h;
 }
 
-void ghough(float *transform, int n_theta, int n_rho, float *grad, int w, int h)
+static void candidate(int ab[2], int w, int h, int i, int j,
+		int n_theta, int n_rho, float gx, float gy, int folding)
+{
+	float x = i - w/2;
+	float y = j - h/2;
+	float theta = M_PI + atan2(gy, gx);
+	float alpha = M_PI + atan2(y, x);
+	float rho = hypot(x, y) * cos(alpha - theta);
+	float ti_theta = n_theta * theta / (2 * M_PI);
+	if (folding && ti_theta > n_theta/2)
+	{
+		ti_theta -= n_theta/2.0;
+		rho *= -1;
+	}
+	if (folding)
+		ti_theta *= 2;
+	int i_rho   = n_rho   * (0.5 + rho   / hypot(w, h) );
+	ab[0] = ti_theta;
+	ab[1] = i_rho;
+	//ab[1] = n_rho * (0.5 + rho / hypot(w, h));
+	//float ti_theta = n_theta * theta / (2 * M_PI);
+	//if (global_fold && ti_theta > n_theta/2)
+	//{
+	//	ti_theta -= n_theta/2.0;
+	//	rho *= -1;
+	//}
+	//int i_rho   = n_rho   * (0.5 + rho   / hypot(w, h) );
+	//if (insideP(n_theta, n_rho, (int)ti_theta, i_rho))
+	//	transform[i_rho*n_theta + (int)ti_theta] += ng;
+}
+
+void ghough(float *transform, int n_theta, int n_rho, float *grad, int w, int h,
+		int folding)
 {
 	// fill counts to zero
 	for (int i = 0; i < n_theta * n_rho; i++)
@@ -26,21 +56,13 @@ void ghough(float *transform, int n_theta, int n_rho, float *grad, int w, int h)
 	for (int i = 0; i < w; i++)
 	{
 		float *g = grad + 2 * (j*w + i);
-		float x = i - w/2;
-		float y = j - h/2;
 		float ng = hypot(g[0], g[1]);
-		float theta = M_PI + atan2(g[1], g[0]);
-		float alpha = M_PI + atan2(y, x);
-		float rho = hypot(x, y) * cos(alpha - theta);
-		float ti_theta = n_theta * theta / (2 * M_PI);
-		if (global_fold && ti_theta > n_theta/2)
-		{
-			ti_theta -= n_theta/2.0;
-			rho *= -1;
-		}
-		int i_rho   = n_rho   * (0.5 + rho   / hypot(w, h) );
-		if (insideP(n_theta, n_rho, (int)ti_theta, i_rho))
-			transform[i_rho*n_theta + (int)ti_theta] += ng;
+		int ab[2];
+		candidate(ab, w, h, i, j, n_theta, n_rho, g[0], g[1], folding);
+		int i_theta = ab[0];
+		int i_rho   = ab[1];
+		if (insideP(n_theta, n_rho, i_theta, i_rho))
+			transform[i_rho*n_theta + i_theta] += ng;
 	}
 }
 
@@ -54,7 +76,11 @@ void ghough(float *transform, int n_theta, int n_rho, float *grad, int w, int h)
 #include "iio.h"
 int main(int c, char *v[])
 {
-	global_fold = pick_option(&c, &v, "f", NULL);
+	// extract optional parameters
+	bool folding    = pick_option(&c, &v, "f", NULL);
+	char *fname_in  = pick_option(&c, &v, "i", "-");
+	char *fname_out = pick_option(&c, &v, "o", "-");
+
 	// process input arguments
 	if (c != 3) {
 		fprintf(stderr, "usage:\n\t"
@@ -67,15 +93,15 @@ int main(int c, char *v[])
 
 	// read input gradient
 	int w, h, pd;
-	float *gradient = iio_read_image_float_vec("-", &w, &h, &pd);
+	float *gradient = iio_read_image_float_vec(fname_in, &w, &h, &pd);
 	if (pd != 2) return fprintf(stderr, "I expect a gradient!\n");
 
 	// compute transform
 	float *transform = malloc(nrho * ntheta * sizeof*transform);
-	ghough(transform, nrho, ntheta, gradient, w, h);
+	ghough(transform, nrho, ntheta, gradient, w, h, folding);
 
 	// save output image
-	iio_save_image_float_vec("-", transform, nrho, ntheta, 1);
+	iio_save_image_float_vec(fname_out, transform, nrho, ntheta, 1);
 
 	// cleanup and exti
 	free(gradient);
