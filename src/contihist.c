2448e741cdd0ef0cea8de82c38b5a1af896c16ee
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2016-01-09 13:54:29 +0100

    [contihist] correct many bugs

--- src/contihist.c
+++ src/contihist.c
@@ -24,6 +24,24 @@ static int ifloatbin(int nbins, float min, float max, float x)
 	return r;
 }
 
+
+#include "xfopen.c"
+static void odump(char *fname, long double (*bin)[2], int n)
+{
+	FILE *f = xfopen(fname, "w");
+	for (int i = 0; i < n; i++)
+		fprintf(f, "%d %Lf %Lf\n", i, bin[i][0], bin[i][1]);
+	xfclose(f);
+}
+
+static void edump(char *fname, long double *e, int n)
+{
+	FILE *f = xfopen(fname, "w");
+	for (int i = 0; i < n; i++)
+		fprintf(f, "%d %Lf\n", i, e[i]);
+	xfclose(f);
+}
+
 void fill_continuous_histogram_fake(long double (*bins)[2], int nbins,
 		float min, float max, float *x, int w, int h)
 {
@@ -67,10 +85,15 @@ int compare_floats(const void *a, const void *b)
 	return (*da > *db) - (*da < *db);
 }
 
-// thi
+
 static void sort_quad(struct quad *q)
 {
 	qsort(q->abcd, 4, sizeof q->abcd[0], compare_floats);
+}
+
+static void sort_four_values(float *x)
+{
+	qsort(x, 4, sizeof*x, compare_floats);
 }
 
 
@@ -91,8 +114,11 @@ static void sort_quad(struct quad *q)
 //	return (fa > fb) - (fa < fb);
 //}
 
-static int degenerateP(float *x)
+#include <stdbool.h>
+static int degenerateP(float min, float max, float *x)
 {
+	if (x[0] < min) return true;
+	if (x[0] > max) return true;
 	return !(x[0] < x[1] && x[1] < x[2] && x[2] < x[3]);
 }
 
@@ -102,10 +128,11 @@ void fill_continuous_histogram(long double (*bins)[2], int nbins,
 	// initialize bins
 	for (int i = 0; i < nbins; i++)
 	{
-		float idi = min + i * (max - min) / nbins;
+		long double idi = min + i * (max - min) / nbins;
 		bins[i][0] = idi;
 		bins[i][1] = 0;
 	}
+	odump("/tmp/dch_full_0.txt", bins, nbins);
 
 	// fill quads
 	struct quad *q = xmalloc(w * h * sizeof*q);
@@ -131,13 +158,17 @@ void fill_continuous_histogram(long double (*bins)[2], int nbins,
 	{
 		// discard degenerate quads
 		float *abcd = q[j*w + i].abcd;
-		if (degenerateP(abcd)) continue;
+		if (degenerateP(min, max, abcd)) continue;
 		nquads += 1;
 
 		// compute bins associated to this quad
 		int iabcd[4];
 		for (int k = 0; k < 4; k++)
 			iabcd[k] = ifloatbin(nbins, min, max, abcd[k]);
+
+		//fprintf(stderr, "full cell (%g %g %g %g)[%d %d %d %d]\n",
+		//		abcd[0], abcd[1], abcd[2], abcd[3],
+		//		iabcd[0], iabcd[1], iabcd[2], iabcd[3]);
 
 		// compute parameters associated to this quad
 		float gamma = 2 / (abcd[3] + abcd[2] - abcd[1] - abcd[0]);
@@ -150,8 +181,11 @@ void fill_continuous_histogram(long double (*bins)[2], int nbins,
 		// accumulate slope increments
 		float dslope_abcd[4] = { alpha, -alpha, beta, -beta};
 		for (int k = 0; k < 4; k++)
+		{
 			e[iabcd[k]] += dslope_abcd[k];
+		}
 	}
+	edump("/tmp/dch_full_1.txt", e, nbins);
 
 	fprintf(stderr, "generic quads = %d / %d (%g%%)\n",
 			nquads, (w-1)*(h-1), nquads*100.0/((w-1)*(h-1)));
@@ -159,13 +193,16 @@ void fill_continuous_histogram(long double (*bins)[2], int nbins,
 	// integrate slope increments (twice)
 	for (int i = 1; i < nbins; i++) e[i] += e[i-1];
 	e[0] = 0;
+	edump("/tmp/dch_full_2.txt", e, nbins);
 	for (int i = 1; i < nbins; i++) e[i] += e[i-1];
 	e[0] = 0;
+	edump("/tmp/dch_full_3.txt", e, nbins);
 
 
 	// copy to output
 	for (int i = 0; i < nbins; i++)
 		bins[i][1] = e[i];
+	odump("/tmp/dch_full_4.txt", bins, nbins);
 
 
 //	struct event *e = xmalloc(w * h * 4 * sizeof*e);
@@ -212,18 +249,146 @@ void fill_continuous_histogram(long double (*bins)[2], int nbins,
 
 }
 
+// copy the values of a square cell into an array, for easy access
+static void copy_cell_values(float q[4], float *x, int w, int h, int i, int j)
+{
+	assert(0 <= i); assert(i < w - 1);
+	assert(0 <= j); assert(j < h - 1);
+	q[0] = x[ (j+0)*w + (i+0) ];
+	q[1] = x[ (j+0)*w + (i+1) ];
+	q[2] = x[ (j+1)*w + (i+0) ];
+	q[3] = x[ (j+1)*w + (i+1) ];
+}
+
+// obtain the histogram bin that corresponds to the given value
+static int bin(int n, float m, float M, float x)
+{
+	float f = (n - 1) * (x - m) / (M - m);
+	int r = lrint(f);
+	assert(m <= x); assert(x <= M); assert(0 <= r); assert(r < n);
+	return r;
+}
+
+static int cell_is_degenerate(float m, float M, float q[4])
+{
+	for (int i = 0; i < 4; i++)
+		if (q[i] < m || q[i] > M)
+			return 1;
+	return !(q[0] < q[1] && q[1] < q[2] && q[2] < q[3]);
+}
+
+static void integrate_values(long double (*o)[2], int n)
+{
+	// TODO : multiply each increment by the span of the interval
+	for (int i = 1; i < n; i++)
+		o[i][1] += o[i-1][1];// * (o[i][0] - o[i-1][0]);
+	//o[0][1] = 0;
+}
+
+static void accumulate_jumps_for_one_cell(long double (*o)[2],
+		int n, float m, float M, float q[4])
+{
+	// discard degenerate cells
+	if (cell_is_degenerate(m, M, q))
+		return;
+
+	// give nice names to numbers
+	float A = q[0];
+	float B = q[1];
+	float C = q[2];
+	float D = q[3];
+	int i_A = bin(n, m, M, A);
+	int i_B = bin(n, m, M, B);
+	int i_C = bin(n, m, M, C);
+	int i_D = bin(n, m, M, D);
+
+	if (i_A == i_B || i_B == i_C || i_C == i_D) return;
+	fprintf(stderr, "simp cell (%g %g %g %g)[%d %d %d %d]\n",
+			A, B, C, D, i_A, i_B, i_C, i_D);
+
+	assert(i_A < i_B);
+	assert(i_B < i_C);
+	assert(i_C < i_D);
+
+	double fac = 33333;
+
+	// accumulate jumps
+	o[ i_A ][1] += fac * 2 / (C + D - B - A) / (B - A);
+	o[ i_B ][1] -= fac * 2 / (C + D - B - A) / (B - A);
+	o[ i_C ][1] -= fac * 2 / (C + D - B - A) / (D - C);
+	o[ i_D ][1] += fac * 2 / (C + D - B - A) / (D - C);
+
+	fprintf(stderr, "\tacc %d %lf\n", i_A, + fac*2/(C + D - B - A)/(B - A));
+	fprintf(stderr, "\tacc %d %lf\n", i_B, - fac*2/(C + D - B - A)/(B - A));
+	fprintf(stderr, "\tacc %d %lf\n", i_C, - fac*2/(C + D - B - A)/(D - C));
+	fprintf(stderr, "\tacc %d %lf\n", i_D, + fac*2/(C + D - B - A)/(D - C));
+}
+
+
+void fill_continuous_histogram_simple(
+	long double (*o)[2], // output histogram array of (value,density) pairs
+	int n,               // requested number of bins for the histogram
+	float m,             // requested minimum of the histogram
+	float M,             // requested maximum of the histogram
+	float *x,            // input image data
+	int w,               // input image width
+	int h                // input image height
+	)
+{
+	// initialize bins (n equidistant points between m and M)
+	for (int i = 0; i < n; i++)
+	{
+		o[i][0] = m + i * (M - m) / n;
+		o[i][1] = 0;
+	}
+	odump("/tmp/dch_simp_0.txt", o, n);
+
+	// compute 2nd derivative of histogram
+	for (int j = 0; j < h - 1; j++)
+	for (int i = 0; i < w - 1; i++)
+	{
+		float q[4];
+		copy_cell_values(q, x, w, h, i, j);
+		sort_four_values(q);
+		accumulate_jumps_for_one_cell(o, n, m, M, q);
+	}
+	odump("/tmp/dch_simp_1.txt", o, n);
+
+	// integrate twice
+	integrate_values(o, n);
+	odump("/tmp/dch_simp_2.txt", o, n);
+	integrate_values(o, n);
+	odump("/tmp/dch_simp_3.txt", o, n);
+}
+
+
+#if  0
+		// compute bins associated to this cell
+		int i_A = ifloatbin(nbins, min, max, A);
+		int i_B = ifloatbin(nbins, min, max, B);
+		int i_C = ifloatbin(nbins, min, max, C);
+		int i_D = ifloatbin(nbins, min, max, D);
+
+		// accumulate bins
+		bin[i_A][1] += 2 / (C + D - B - A) / (B - A);
+		bin[i_B][1] -= 2 / (C + D - B - A) / (B - A);
+		bin[i_A][1] += 2 / (C + D - B - A) / (D - C);
+		bin[i_B][1] -= 2 / (C + D - B - A) / (D - C);
+#endif
+
+
 void accumulate_histogram(long double (*h)[2], int n)
 {
 	for (int i = 1; i < n; i++)
 		h[i][1] += h[i-1][1];
 }
 
-void dump_histogram(long double (*h)[2], int n)
+void dump_histogram(FILE *f, long double (*h)[2], int n)
 {
 	long double (*a)[2] = xmalloc(n*sizeof*a);
 	memcpy(a, h, n*sizeof*a);
 	accumulate_histogram(a, n);
-	printf("set xrange [%Lg:%Lg]\n", h[0][0], h[n-1][0]);
+	fprintf(f, "set xrange [%Lg:%Lg]\n", h[0][0], h[n-1][0]);
 	//printf("set yrange [0:]\n");
 	//printf("set format y \"\"\n");
 	//if (SHOWSTATS() > 0) {
@@ -235,28 +400,28 @@ void dump_histogram(long double (*h)[2], int n)
 	//	//printf("C(m,s,x)=1/((1+((x-m)/s)**2)*(pi*s))\n");
 	//}
 	//else
-		printf("unset key\n");
-	printf("plot \"-\" w impulses title \"histogram\", \"-\" w lines title \"accumulated histogram\",0");
+		fprintf(f, "unset key\n");
+	fprintf(f, "plot \"-\" w impulses title \"histogram\", \"-\" w lines title \"accumulated histogram\",0");
 	//if (SHOWSTATS() > 0) {
 	//	void print_gnuplot_stats_string(long double (*)[2], int, float);
 	//	print_gnuplot_stats_string(h, n, 1/SHOWSTATS());
 	//}
-	printf("\n");
+	fprintf(f, "\n");
 	long double m = 0;
 	for (int i = 0; i < n; i++)
 	{
 		if (h[i][1] > m) m = h[i][1];
-		printf("\t%Lg\t%Lg\n", h[i][0], h[i][1]);
+		fprintf(f, "\t%Lg\t%Lg\n", h[i][0], h[i][1]);
 	}
-	printf("end\n");
-	long double f = m/a[n-1][1];
+	fprintf(f, "end\n");
+	long double fac = m/a[n-1][1];
 	//fprintf(stderr, "m = %Lg\n", m);
 	//fprintf(stderr, "f = %Lg\n", f);
 	//fprintf(stderr, "a = %Lg\n", a[n-1][1]);
 	//fprintf(stderr, "n = %d\n", n);
 	for (int i = 0; i < n; i++)
-		printf("\t%Lg\t%Lg\n", a[i][0], f*a[i][1]);
-	printf("end\n");
+		fprintf(f, "\t%Lg\t%Lg\n", a[i][0], fac*a[i][1]);
+	fprintf(f, "end\n");
 	free(a);
 }
 
@@ -324,13 +489,14 @@ int main(int c, char *v[])
 	float *x = iio_read_image_float(filename_in, &w, &h);
 
 	// allocate space for the histogram data
-	long double bins[nbins][2];
+	long double bins[3+nbins][2];
 
 	// compute continuous histogram
 	fill_continuous_histogram(bins, nbins, hmin, hmax, x, w, h);
+	fill_continuous_histogram_simple(bins, nbins, hmin, hmax, x, w, h);
 
 	// dump histogram to stdout
-	dump_histogram(bins, nbins);
+	dump_histogram(stdout, bins, nbins);
 
 	// cleanup and exit
 	free(x);
