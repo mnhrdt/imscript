db52430e18a53505870ce3480fb0688c9e65f4f5
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2016-09-25 15:33:26 +0200

    add image T (useful for pipelines)

---
+++ other/itee.c
@@ -0,0 +1,23 @@
+// itee, copy stdin to stdout, and open an image (using fpan) with this data
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char fname[] = "/tmp/itee_XXXXXX";
+	int fd = mkstemp(fname);
+	FILE *f = fdopen(fd, "w");
+	int c;
+	while ((c = getchar()) != EOF)
+	{
+		putchar(c);
+		fputc(c, f);
+	}
+	putchar(EOF);
+	fclose(f);
+	char buf[0x100];
+	sprintf(buf, "fpan %s &", fname);
+	system(buf);
+	return 0;
+}
