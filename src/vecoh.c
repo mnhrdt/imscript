1bc9f70f9517ea05f5451fed4f71f9970067b4ce
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2017-03-06 10:53:47 +0100

    k-means with variance-based tuning for k
    [ci skip]

--- src/vecoh.c
+++ src/vecoh.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -173,25 +174,6 @@ static float fdiste(float *x, float *y, int n, float e)
 
 #include "smapa.h"
 
-// y[k] = euclidean median of the vectors x[i][k]
-static void float_weisz(float *y, float *x, int d, int n)
-{
-	float_avg(y, x, d, n);
-	int niter = WEISZ_NITER();
-	for (int k = 0; k < niter; k++) {
-		float a[d], b = 0;
-		for (int l = 0; l < d; l++)
-			a[l] = 0;
-		for (int i = 0; i < n; i++) {
-			float dxy = fdiste(x + i*d, y, d, 1e-5);
-			for (int l = 0; l < d; l++)
-				a[l] += x[i*d + l]/dxy;
-			b += 1/dxy;
-		}
-		for (int l = 0; l < d; l++)
-			y[l] = a[l]/b;
-	}
-}
 
 int compare_floats(const void *a, const void *b)
 {
@@ -200,34 +182,74 @@ int compare_floats(const void *a, const void *b)
 	return (*da > *db) - (*da < *db);
 }
 
-static float float_med(float *x, int n)
+//static float float_med(float *x, int n)
+//{
+//	if (!n) return NAN;//fail("empty list of pixel values!");
+//	if (n == 1) return x[0];
+//	if (n == 2) return x[0];
+//	qsort(x, n, sizeof*x, compare_floats);
+//	return x[n/2];
+//}
+
+static float average_of_cluster_variances(
+		int *group, float *mean, int k,
+		float *x, int n)
 {
-	if (!n) return NAN;//fail("empty list of pixel values!");
-	if (n == 1) return x[0];
-	if (n == 2) return x[0];
-	qsort(x, n, sizeof*x, compare_floats);
-	return x[n/2];
+	float var[k], cnt[k];
+	for (int i = 0; i < k; i++)
+		var[i] = cnt[i] = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		float vi = x[i];
+		int gi = group[i];
+		assert(gi >= 0);
+		assert(gi <  k);
+		var[gi] += (x[i] - mean[gi]) * (x[i] - mean[gi]);
+		cnt[gi] += 1;
+	}
+	for (int i = 0; i < k; i++)
+		var[i] /= cnt[i];
+
+	float r = 0;
+	for (int i = 0; i < k; i++)
+		r += var[i];
+	return r / k;
 }
 
+static void assign_each_point_to_nearest_mean(int *assign, float *mean,
+		int k, float *x, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int bj = 0;
+		for (int j = 0; j < k; j++)
+			if (fabs(x[i]-mean[j]) < fabs(x[i]-mean[bj]))
+				bj = j;
+		assign[i] = bj;
+	}
+}
 
-static void kmeans_1d(float *means, int *x, int n, int k)
+static void kmeans_1d(float *means, float *x, int n, int k)
 {
 	// order and sanity
 	for (int i = 0; i < k; i++)
 		means[i] = NAN;
 	if (!n) return;
 	qsort(x, n, sizeof*x, compare_floats);
-	if (n <= k)
+	if (n <= k) {
 		for (int i = 0; i < n; i++)
 			means[i] = x[i];
+		return;
+	}
 
 	// initialize the position of the clusters uniformly
-	assert(k > n);
+	assert(n > k);
 	for (int i = 0; i < k; i++)
 	{
 		int idx = lrint(  (i + 1.0) * (n - 1.0) / (k + 1.0)  );
 		assert(idx >= 0);
-		assert(idx < 0);
+		assert(idx < n);
 		means[i] = x[idx];
 	}
 
@@ -263,6 +285,8 @@ static void kmeans_1d(float *means, int *x, int n, int k)
 
 }
 
+SMART_PARAMETER_SILENT(VECOH_K,2)
+
 // y[0] = number of modes of the samples x[i] (must satisfy y[0] < dy-1)
 // y[1] = position of first mode
 // y[2] = position of second mode
@@ -273,8 +297,45 @@ static void kmeans_1d(float *means, int *x, int n, int k)
 // y[dy-1] = nan
 static void float_kmeans(float *y, int dy, float *x, int n)
 {
+	int k = VECOH_K();
+	if (k >= dy) k = dy - 1;
+	float means[k];
+	kmeans_1d(means, x, n, k);
+	int count = 0;
+	for (int i = 0; i < k; i++)
+		count += isfinite(means[i]);
+	y[0] = count;
+	int ypos = 1;
+	for (int i = 0; i < k; i++)
+		if (isfinite(means[i]))
+			y[ypos++] = means[i];
 }
 
+static int main_kmeans(int c, char *v[])
+{
+	if(c<3)return fprintf(stderr,"usage:\n\tVECOH_K=2 kmeans x1 ... xn\n");
+	int n = c - 1;
+	float x[n], y[n];
+	for (int i = 0; i < n; i++)
+		x[i] = atof(v[i+1]);
+	float_kmeans(y, 20, x, n);
+	int k = y[0];
+	//for (int i = 0; i < k; i++)
+	//	fprintf(stderr, "center[%d] = %g\n", i+1, y[1+i]);
+	int group[n];
+	assign_each_point_to_nearest_mean(group, y+1, k, x, n);
+	//for(int i = 0; i < n; i++)fprintf(stderr,"x[%d] = %g\n",i,x[i]);
+	//for(int i = 0; i < n; i++)fprintf(stderr,"g[%d] = %d\n",i,group[i]);
+	float acv = average_of_cluster_variances(group, y+1, k, x, n);
+	fprintf(stderr, "racv = %g\t", sqrt(acv));
+	fprintf(stderr, "got %d centers (", k);
+	for (int i = 0; i < k; i++)
+		fprintf(stderr, "%g%c", y[1+i], i+1==k?')':' ');
+	puts("");
+	return 0;
+}
+
+int main(int c, char **v) { return main_kmeans(c, v); }
 
 static bool isgood(float *x, int n)
 {
@@ -286,52 +347,52 @@ static bool isgood(float *x, int n)
 
 #include "pickopt.c"
 
-int main_vecov(int c, char *v[])
-{
-	char *filename_out = pick_option(&c, &v, "o", "-");
-	if (c < 4) {
-		fprintf(stderr,
-		"usage:\n\t%s {kmeans|modes} [v1 ...] [-o out]\n", *v);
-		//          0  1              2  3
-		return EXIT_FAILURE;
-	}
-	int n = c - 2;
-	char *operation_name = v[1];
-	void (*f)(float*,float*,int,int) = NULL;
-	if (0 == strcmp(operation_name, "kmeans")) f = float_kmeans;
-	if (0 == strcmp(operation_name, "modes"))  f = float_modes;
-	if (!f) fail("unrecognized operation \"%s\"", operation_name);
-	float *x[n];
-	int w[n], h[n], pd[n];
-	for (int i = 0; i < n; i++)
-		x[i] = iio_read_image_float_vec(v[i+2], w + i, h + i, pd + i);
-	for (int i = 0; i < n; i++) {
-		if (w[i] != *w || h[i] != *h || pd[i] != *pd)
-			fail("%dth image sizes mismatch\n", i);
-	}
-	int out_pd = *pd;
-	float (*y) = xmalloc(*w * *h * out_pd * sizeof*y);
-#ifdef _OPENMP
-#pragma omp parallel for
-#endif
-	for (int i = 0; i < *w * *h; i++) {
-		float tmp[n][*pd];
-		int ngood = 0;
-		for (int j = 0; j < n; j++)
-			if (isgood(x[j]+i**pd, *pd)) {
-				for (int k = 0; k < *pd; k++)
-					tmp[ngood][k] = x[j][i**pd+k];
-				ngood += 1;
-			}
-		f(y + i**pd, tmp[0], *pd, ngood);
-	}
-	iio_write_image_float_vec(filename_out, y, *w, *h, *pd);
-	free(y);
-	for (int i = 0; i < n; i++)
-		free(x[i]);
-	return EXIT_SUCCESS;
-}
+//int main_vecov(int c, char *v[])
+//{
+//	char *filename_out = pick_option(&c, &v, "o", "-");
+//	if (c < 4) {
+//		fprintf(stderr,
+//		"usage:\n\t%s {kmeans|modes} [v1 ...] [-o out]\n", *v);
+//		//          0  1              2  3
+//		return EXIT_FAILURE;
+//	}
+//	int n = c - 2;
+//	char *operation_name = v[1];
+//	void (*f)(float*,float*,int,int) = NULL;
+//	if (0 == strcmp(operation_name, "kmeans")) f = float_kmeans;
+//	if (0 == strcmp(operation_name, "modes"))  f = float_modes;
+//	if (!f) fail("unrecognized operation \"%s\"", operation_name);
+//	float *x[n];
+//	int w[n], h[n], pd[n];
+//	for (int i = 0; i < n; i++)
+//		x[i] = iio_read_image_float_vec(v[i+2], w + i, h + i, pd + i);
+//	for (int i = 0; i < n; i++) {
+//		if (w[i] != *w || h[i] != *h || pd[i] != *pd)
+//			fail("%dth image sizes mismatch\n", i);
+//	}
+//	int out_pd = *pd;
+//	float (*y) = xmalloc(*w * *h * out_pd * sizeof*y);
+//#ifdef _OPENMP
+//#pragma omp parallel for
+//#endif
+//	for (int i = 0; i < *w * *h; i++) {
+//		float tmp[n][*pd];
+//		int ngood = 0;
+//		for (int j = 0; j < n; j++)
+//			if (isgood(x[j]+i**pd, *pd)) {
+//				for (int k = 0; k < *pd; k++)
+//					tmp[ngood][k] = x[j][i**pd+k];
+//				ngood += 1;
+//			}
+//		f(y + i**pd, tmp[0], *pd, ngood);
+//	}
+//	iio_write_image_float_vec(filename_out, y, *w, *h, *pd);
+//	free(y);
+//	for (int i = 0; i < n; i++)
+//		free(x[i]);
+//	return EXIT_SUCCESS;
+//}
 
-#ifndef HIDE_ALL_MAINS
-int main(int c, char **v) { return main_vecov(c, v); }
-#endif
+//#ifndef HIDE_ALL_MAINS
+//int main(int c, char **v) { return main_vecov(c, v); }
+//#endif
