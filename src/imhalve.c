df56b0b0ee96c0b46e1500978b6b98304452a0a6
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2020-01-30 10:53:16 +0100

    imhalve simplify code

--- src/imhalve.c
+++ src/imhalve.c
@@ -1,15 +1,13 @@
 // cut the first quarter of an image
-#include <stdlib.h> // malloc
 #include "iio.h"
 int main(void)
 {
 	int w, h, pd;
 	float *x = iio_read_image_float_vec("-", &w, &h, &pd);
-	float *y = malloc(w*h*pd*sizeof*y);
 	for (int j = 0; j < h/2; j++)
 	for (int i = 0; i < w/2; i++)
 	for (int l = 0; l < pd; l++)
-		y[pd*(j*(w/2)+i)+l] = x[pd*(j*w+i)+l];
-	iio_write_image_float_vec("-", y, w/2, h/2, pd);
+		x[pd*(j*(w/2)+i)+l] = x[pd*(j*w+i)+l];
+	iio_write_image_float_vec("-", x, w/2, h/2, pd);
 	return 0;
 }
