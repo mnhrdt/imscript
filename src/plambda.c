mnhrdt/imscript

commit 26be7bf71c7ec0cacaf45c251f1f4f74bb85c99d

[plambda] nstack off by one bug

src/plambda.c
@@ -1991,7 +1991,7 @@ static void vstack_process_op(struct value_vstack *s, int opid)
 				     }
 		break;
 	case PLAMBDA_STACKOP_NSTACK:
-		vstack_push_scalar(s, s->n);
+		vstack_push_scalar(s, s->n - 1);
 		break;
 	case PLAMBDA_STACKOP_VMERGE: {
 		float x[PLAMBDA_MAX_PIXELDIM];

commit e3bbcc2abc71d3e53e9d1f1fcd7d2d3c5ec5812c

fix bug introduced in 26be7bf71c

src/plambda.c
@@ -2098,7 +2098,7 @@ static void vstack_process_op(struct value_vstack *s, int opid)
 				     }
 		break;
 	case PLAMBDA_STACKOP_NSTACK:
-		vstack_push_scalar(s, s->n - 1);
+		vstack_push_scalar(s, s->n);
 		break;
 	case PLAMBDA_STACKOP_VMERGE: {
 		float x[PLAMBDA_MAX_PIXELDIM];