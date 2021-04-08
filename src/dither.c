a2a20e8acd920d0589fa2104086a8c3b98b47a89
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2021-04-08 20:15:09 +0200

    [dither.c] -b: boustrophedonic Floyd-Steinberg

--- src/dither.c
+++ src/dither.c
@@ -55,15 +55,48 @@ void dither(float *x, int w, int h)
 	}
 }
 
-void dither_sep(float *x, int w, int h, int pd)
+// boustrophedonic Floyd-Steinberg
+void dither_b(float *x, int w, int h)
+{
+	getpixel_operator get = getpixel_127;
+	setpixel_operator set = setpixel_insideP;
+	setpixel_operator add = setpixel_tsum_insideP;
+
+	for (int j = 0; j < h; j++)
+	for (int i = 0; i < w; i++)
+	{
+		int ii = (j%2) ? w - i - 1 : i;
+		float old = get(x, w, h, ii, j);
+		float new = dither_value(old);
+		float err = old - new;
+		set(x, w, h, ii, j, new);
+		if (j%2) {
+			add(x, w, h, ii-1, j+0, 7*err/16);
+			add(x, w, h, ii+1, j+1, 3*err/16);
+			add(x, w, h, ii-0, j+1, 5*err/16);
+			add(x, w, h, ii-1, j+1, 1*err/16);
+		} else {
+			add(x, w, h, ii+1, j+0, 7*err/16);
+			add(x, w, h, ii-1, j+1, 3*err/16);
+			add(x, w, h, ii+0, j+1, 5*err/16);
+			add(x, w, h, ii+1, j+1, 1*err/16);
+		}
+	}
+}
+
+void dither_sep(float *x, int w, int h, int pd, _Bool b)
 {
 	for (int i = 0; i < pd; i++)
-		dither(x + i*w*h, w, h);
+		if (b)
+			dither_b(x + i*w*h, w, h);
+		else
+			dither(x + i*w*h, w, h);
 }
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "iio.h"
+#include "pickopt.c"
 int main_dither(int c, char *v[])
 {
 	if (c != 1 && c != 2 && c != 3) {
@@ -71,13 +104,14 @@ int main_dither(int c, char *v[])
 		//                          0  1     2
 		return 1;
 	}
+	_Bool option_b = pick_option(&c, &v, "b", 0);
 	char *filename_in  = c > 1 ? v[1] : "-";
 	char *filename_out = c > 2 ? v[2] : "-";
 
 	int w, h, pd;
 	float *x = iio_read_image_float_split(filename_in, &w, &h, &pd);
 
-	dither_sep(x, w, h, pd);
+	dither_sep(x, w, h, pd, option_b);
 
 
 	iio_write_image_float_split(filename_out, x, w, h, pd);
