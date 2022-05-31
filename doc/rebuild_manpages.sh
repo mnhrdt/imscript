c2bf76356483a215d1aca2a5d4ea03afb76f28b6
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2022-05-31 11:11:00 +0200

    manpage script

--- doc/rebuild_manpages.sh
+++ doc/rebuild_manpages.sh
@@ -1,4 +1,5 @@
-help2man --help >/dev/null && for i in `ls ../src/*.c|grep -v help_stuff`; do
+help2man --help >/dev/null &&\
+for i in `ls ../src/*.c ../src/ftr/*.c|grep -v help_stuff`; do
 	X=`basename $i .c`
 	grep -q string_oneliner $i && ../bin/$X --manraw |\
 		sed 's/by help2man 1.*/by help2man/' > man/$X.1
