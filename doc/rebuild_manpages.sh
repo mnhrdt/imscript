422c0d7872f61bc42e0bc77c214911d0271f275c
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2023-01-23 11:14:15 +0100

    relocate manpages
    Now you can do
    export MANPATH=/path/to/imscript/doc/man:$MANPATH
    and see the man pages with regular man

--- doc/rebuild_manpages.sh
+++ doc/rebuild_manpages.sh
@@ -4,6 +4,6 @@ help2man --help >/dev/null &&\
 for i in `ls ../src/*.c ../src/ftr/*.c|grep -v help_stuff`; do
 	X=`basename $i .c`
 	grep -q string_oneliner $i && ../bin/$X --manraw |\
-		sed 's/by help2man 1.*/by help2man/' > man/$X.1
+		sed 's/by help2man 1.*/by help2man/' > man/man1/$X.1
 done
 true
