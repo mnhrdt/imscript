5203dd2b514a9581e1313a279a22464b38fb2f8e
enric meinhardt <enric.meinhardt@ens-paris-saclay.fr>
2023-02-17 16:45:08 +0100

    separate python and octave stuff

---
+++ src/python/setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+
+setuptools.setup(
+	name = "cpu",
+	version = "0",
+	author = "Enric Meinhardt-Llopis",
+	author_email = "enric.meinhardt@fastmail.com",
+	description = "The Definitive Interface for Image Processing Practitioners",
+	url = "https://github.com/mnhrdt/imscript",
+	classifiers = [
+		"Operating System :: OS Independent",
+		"License :: OSI Approved :: GNU Affero General Public License v3",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Mathematics"
+		],
+	py_modules = ["cpu"]
+)
