dce48bd1ae1ff0dbf49bd275ebc4ea3f658ee7ba
Enric Meinhardt-Llopis <enric.meinhardt@ens-paris-saclay.fr>
2023-07-01 23:58:08 +0200

    setup.py: install all core packages

--- src/python/imscript/setup.py
+++ src/python/imscript/setup.py
@@ -1,17 +1,22 @@
 
+def programs():
+	from subprocess import check_output as sh
+	return sh("make -np|grep BIN\ :|cut -c8-", shell=True, text=True)
+
+
 from setuptools.command.install import install
 class myinstall(install):
 	def run(self):
 		import os
 		os.system("mkdir -p bin")
-		os.system("make -j bin/plambda bin/qauto bin/morsi")
+		os.system(f"make -j {programs()}")
 		install.run(self)
 
 
 import setuptools
 setuptools.setup(
 	name = "imscript",
-	version = "0.7",
+	version = "0.8",
 	author = "Enric Meinhardt-Llopis",
 	author_email = "enric.meinhardt@fastmail.com",
 	description = "Image Processing with Unix Pipes",
@@ -23,5 +28,5 @@ setuptools.setup(
 		"Topic :: Scientific/Engineering :: Mathematics"
 		],
 	cmdclass = {'install' : myinstall},
-	data_files = [('bin', ['bin/plambda', 'bin/qauto', 'bin/morsi'])]
+	data_files = [('bin', programs().split())]
 )
