42de13f4b8f33dd5e07c44bb99009dae9bb706da
enric meinhardt-llopis <enric.meinhardt@ens-paris-saclay.fr>
2024-12-10 17:00:58 +0100

    points.c: scaffolding for polytopes

--- src/points.c
+++ src/points.c
@@ -38,6 +38,113 @@
 
 #include "random.c"
 
+
+
+// struct tag to hold the data for a multidimensional wireframe
+struct wireframe {
+	int d;     // dimension
+	int n;     // number of points
+	int m;     // number of edges
+	float *x;  // coordinates of the points (d*n floats)
+	int *e;  // indices of the edges (2*m ints)
+};
+
+static void fprint_wireframe(FILE *f, struct wireframe *w)
+{
+	fprintf(f, "wireframe\n");
+	fprintf(f, "d = %d\n", w->d);
+	fprintf(f, "n = %d\n", w->n);
+	fprintf(f, "m = %d\n", w->m);
+	fprintf(f, "x =\n");
+	for (int j = 0; j < w->n; j++)
+	{
+		for (int i = 0; i < w->d; i++)
+			fprintf(f, "\t%g", w->x[j*w->d+i]);
+		fprintf(f, "\n");
+	}
+	for (int j = 0; j < w->m; j++)
+		fprintf(f, "\t%d\t%d\n", w->e[2*j+0], w->e[2*j+1]);
+}
+
+// replace each edge of a wireframe with s>=3 uniformly distributed points
+static void unwireframize(struct wireframe *w, int s)
+{
+	assert(s >= 3);
+
+	// the original points are kept, for each edge we add s-2 points inside
+	int n = w->n + (s - 2) * w->m;
+	float *x = xmalloc(n * w->d * sizeof*x);
+	memcpy(x, w->x, w->n * w->d * sizeof*x);
+	for (int j = 0; j < w->m; j++)
+	{
+		int a = w->e[2*j+0];
+		int b = w->e[2*j+1];
+		float *p = x + a * w->d;
+		float *q = x + b * w->d;
+		for (int k = 1; k < s - 1; k++)
+		{
+			float t = k/(s-1.0);
+			float *y = x + w->n * w->d;
+			for (int i = 0; i < w->d; i++)
+				y[i] = t*p[i] + (1 - t)*q[i];
+			w->n += 1;
+		}
+	}
+
+	// remove previous edges
+	xfree(w->e);
+	w->m = 0;
+	w->e = NULL;
+
+	// record new points
+	xfree(w->x);
+	w->n = n;
+	w->x = x;
+}
+
+static void build_positive_axes(struct wireframe *w, int d)
+{
+	w->d = d;         // dimension
+	w->n = d + 1;     // one point per dimension, plus the center 0
+	w->m = d;         // one edge per dimension
+	w->x = xmalloc(w->n * d * sizeof*w->x);
+	w->e = xmalloc(w->m * 2 * sizeof*w->e);
+	for (int i = 0; i < w->n * d; i++) // zero everything
+		w->x[i] = 0;
+	for (int j = 0; j < d; j++)        // jth component of x_j is 1
+		w->x[d*j + j] = 1;
+	for (int j = 0; j < d; j++)        // edges from 0 to each e_j
+	{
+		w->e[2*j+0] = d;           // index of 0
+		w->e[2*j+1] = j;           // index of e_j
+	}
+}
+
+static void build_centered_axes(struct wireframe *w, int d)
+{
+	w->d = d;          // dimension
+	w->n = 2*d + 1;    // two points per axis, plus the 0
+	w->m = 2*d;        // two edges per axis
+	w->x = xmalloc(w->n * d * sizeof*w->x);
+	w->e = xmalloc(w->m * 2 * sizeof*w->e);
+	for (int i = 0; i < w->n * d; i++)  // zero all
+		w->x[i] = 0;
+	for (int j = 0; j < d; j++)
+	{
+		w->x[d*(2*j+0) + j] = 1;
+		w->x[d*(2*j+1) + j] = -1;
+	}
+	for (int j = 0; j < d; j++)
+	{
+		w->e[2*(2*j+0)+0] = 2*d;
+		w->e[2*(2*j+0)+1] = 2*j + 0;
+		w->e[2*(2*j+1)+0] = 2*d;
+		w->e[2*(2*j+1)+1] = 2*j + 1;
+	}
+}
+
+// TODO: add builders for all regular polytopes in dimensions 4 and above
+
 static void print_points(float *x, int n, int d)
 {
 	for (int i = 0; i < n; i++)
@@ -141,6 +248,30 @@ int main_map(int c, char *v[])
 	return 0;
 }
 
+int main_polytope(int c, char *v[])
+{
+	// named arguments
+	int s = atoi(pick_option(&c, &v, "s", "10"));
+	if (!s) return fprintf(stderr, "please put an s, not %d\n", s);
+
+	// positional arguments
+	if (c != 3)
+		return fprintf(stderr, "usage\n\t%s dim type\n", *v);
+		//                                0 1   2
+
+	int d = atoi(v[1]); // dimension
+	char *poly_type = v[2];
+
+	struct wireframe p[1];
+	//build_positive_axes(p, d);
+	build_centered_axes(p, d);
+	//fprint_wireframe(stderr, p);
+	unwireframize(p, s);
+	print_points(p->x, p->n, d);
+
+	return 0;
+}
+
 // special configurations
 int main_config(int c, char *v[])
 {
@@ -200,6 +331,7 @@ int main_points(int c, char *v[])
 	else if (0 == strcmp(v[1], "random")) return main_random(c-1, v+1);
 	else if (0 == strcmp(v[1], "map")) return main_map(c-1, v+1);
 	else if (0 == strcmp(v[1], "config")) return main_config(c-1, v+1);
+	else if (0 == strcmp(v[1], "polytope")) return main_polytope(c-1, v+1);
 //	else if (0 == strcmp(v[1], "stats")) return main_stats(c-1, v+1);
 	else {
 	usage: fprintf(stderr, "usage:\n\t%s [random|config|stats] "
