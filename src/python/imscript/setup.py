d4d261a1e06f88419a482dd13160f3d3f0fe0dff
Enric Meinhardt-Llopis <enric.meinhardt@ens-paris-saclay.fr>
2025-02-14 16:34:10 +0100

    imscripts pypi distribution: fix weird things

--- src/python/imscript/setup.py
+++ src/python/imscript/setup.py
@@ -1,22 +1,22 @@
 
 def programs():
 	from subprocess import check_output as sh
-	return sh("make -np|grep BIN\ :|cut -c8-", shell=True, text=True)
+	return sh("make -np|grep BIN.:|cut -c8-", shell=True, text=True)
 
 
-from setuptools.command.install import install
-class myinstall(install):
+from setuptools.command.install import install as _install
+class myinstall(_install):
 	def run(self):
 		import os
 		os.system("mkdir -p bin")
-		os.system(f"make -j {programs()}")
-		install.run(self)
+		os.system(f"make -j 10 {programs()}")
+		_install.run(self)
 
 
 import setuptools
 setuptools.setup(
 	name = "imscript",
-	version = "0.82",
+	version = "2",
 	author = "Enric Meinhardt-Llopis",
 	author_email = "enric.meinhardt@fastmail.com",
 	description = "Image Processing with Unix Pipes",
